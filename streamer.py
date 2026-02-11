be5f54ff0cc02d51220970cfb34885075eeaf61d
Richard Zhang <richardzhangzzx@gmail.com>
2026-02-10 22:11:56 -0600

    Finish part 2

--- streamer.py
+++ streamer.py
@@ -3,6 +3,10 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 
+import struct
+
+HEADER_FORMAT = "!I"
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -13,24 +17,46 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        # Seq for keeping order
+        self.send_seq = 0
+        self.expected_seq = 0
+        self.recv_buffer = {}
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        # Your code goes here!  The code below should be changed!
-
         # for now I'm just sending the raw application-level data in one UDP payload
         for i in range(0, len(data_bytes), 1472):
             offset = min(i+1472, len(data_bytes))
-            self.socket.sendto(data_bytes[i:offset], (self.dst_ip, self.dst_port))
+            payload = data_bytes[i:offset]
+            header = struct.pack(HEADER_FORMAT, self.send_seq)
+            packet = header + payload
+
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+
+            self.send_seq += 1 
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-        
+        """Blocks (waits) if no data is ready to be read from the connection."""        
         # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        
+        while True:
+            data, addr = self.socket.recvfrom() # Return a packet
+            
+            header = data[:HEADER_SIZE]
+            payload = data[HEADER_SIZE:]
+            
+            (seq,) = struct.unpack(HEADER_FORMAT, header) 
+            
+            self.recv_buffer[seq] = payload
+
+            if self.expected_seq in self.recv_buffer:
+                out = self.recv_buffer.pop(self.expected_seq) # out is the payload
+                self.expected_seq += 1
+                return out
+        
+            
+
+
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
