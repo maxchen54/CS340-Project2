8a01df5a0e226ce09cb1be277d738246c7e56512
Richard Zhang <richardzhangzzx@gmail.com>
2026-02-15 01:02:49 -0600

    Finish part 3

--- streamer.py
+++ streamer.py
@@ -9,7 +9,7 @@ import threading
 import time
 
 HEADER_FORMAT = "!BI" # B: 1 byte for packet type, I: 4 bytes for sequence number
-# We will set 0=Data packet, 1=ACK packet
+# We will set 0=Data packet, 1=ACK packet, 2=FIN Packet, 3=FIN-ACK packet
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
 
@@ -29,6 +29,8 @@ class Streamer:
         self.closed = False
 
         self.ack = True # Initialized to true so first packet can be sent
+        self.fin = False 
+        self.fin_ack = False
 
         self.lock = threading.Lock()
         self.condval = threading.Condition(self.lock)
@@ -62,6 +64,17 @@ class Streamer:
                 elif type == 1: # ACK packet, send nothing
                     with self.lock:
                         self.ack = True
+
+                elif type == 2: # FIN packet, send FIN-ACK back
+                    fin_ack_header = struct.pack(HEADER_FORMAT, 3, seq)
+                    self.socket.sendto(fin_ack_header, addr)
+                    with self.lock: 
+                        self.fin = True 
+
+                elif type == 3: # FIN-ACK packet
+                    with self.lock:
+                        self.fin_ack = True
+
             except Exception as e:
                 print("Listener died!")
                 print(e)
@@ -75,15 +88,20 @@ class Streamer:
             header = struct.pack(HEADER_FORMAT, 0, self.send_seq) # 0 indicate this packet is data, not ack
             packet = header + payload
 
-            # Wait for ACK from previous packet
-            with self.lock: 
-                while not self.ack:
-                    self.lock.release()
-                    time.sleep(0.01)
-                    self.lock.acquire()
-                
-                self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-                self.ack = False
+            # Timeout for waiting for ACKs
+            self.ack = False 
+            timeout = 0.25
+            start_time = time.time()
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port)) 
+
+            # Wait for ACK from the send above
+            while not self.ack:
+                if time.time() - start_time > timeout: 
+                    # Exceed timeout, resend packet (retransmission)
+                    self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+                    start_time = time.time() # Reset Timer
+                time.sleep(0.01)
+            
             self.send_seq += 1
 
     def recv(self) -> bytes:
@@ -103,7 +121,29 @@ class Streamer:
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
-        # your code goes here, especially after you add ACKs and retransmissions.
+        # Send a FIN packet
+        self.fin_ack = False
+        fin_header = struct.pack(HEADER_FORMAT, 2, self.send_seq)
+        timeout = 0.25
+        start_time = time.time()
+        self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
+
+        # Wait for FIN-ACK
+        while not self.fin_ack:
+            if time.time() - start_time > timeout:
+                self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
+                start_time = time.time()
+            time.sleep(0.01)
+
+        self.send_seq += 1
+
+        # Wait for FIN from the other side, this is different than wait for FIN-ACK
+        while not self.fin:
+            time.sleep(0.01)
+    
+        time.sleep(2)
+        
+        # Stop the listener thread
         self.closed = True
         self.socket.stoprecv()
 
