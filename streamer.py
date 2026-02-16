0e9fe5cd7de1b2fbd7e56d6076f9a02986be8db8
Richard Zhang <richardzhangzzx@gmail.com>
2026-02-16 11:26:38 -0600

    Finish part 4

--- streamer.py
+++ streamer.py
@@ -7,8 +7,9 @@ import struct
 import concurrent.futures
 import threading
 import time
+import hashlib
 
-HEADER_FORMAT = "!BI" # B: 1 byte for packet type, I: 4 bytes for sequence number
+HEADER_FORMAT = "!BI16s" # B: 1 byte for packet type, I: 4 bytes for sequence number
 # We will set 0=Data packet, 1=ACK packet, 2=FIN Packet, 3=FIN-ACK packet
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
@@ -50,23 +51,31 @@ class Streamer:
                 header = data[:HEADER_SIZE]
                 payload = data[HEADER_SIZE:]
 
-                (type, seq) = struct.unpack(HEADER_FORMAT, header)
+                (type, seq, hash) = struct.unpack(HEADER_FORMAT, header)
 
                 if type == 0: # Data packet, send ACK Back
-                    ack_header = struct.pack(HEADER_FORMAT, 1, seq)
-                    self.socket.sendto(ack_header, addr)
+                    # Check hash result
+                    check_hash = hashlib.md5(payload).digest()
 
-                    # Store data in buffer
-                    with self.condval:
-                        self.recv_buffer[seq] = payload
-                        self.condval.notify_all()
+                    # Only buffer packet if hash matches. Otherwise do nothing  
+                    if check_hash == hash:
+                        # Include an empty hash for non-data packets
+                        empty_hash = hashlib.md5(b'').digest()
+                        ack_header = struct.pack(HEADER_FORMAT, 1, seq, empty_hash)
+                        self.socket.sendto(ack_header, addr)
+
+                        # Store data in buffer
+                        with self.condval:
+                            self.recv_buffer[seq] = payload
+                            self.condval.notify_all()
 
                 elif type == 1: # ACK packet, send nothing
                     with self.lock:
                         self.ack = True
 
                 elif type == 2: # FIN packet, send FIN-ACK back
-                    fin_ack_header = struct.pack(HEADER_FORMAT, 3, seq)
+                    empty_hash = hashlib.md5(b'').digest()
+                    fin_ack_header = struct.pack(HEADER_FORMAT, 3, seq, empty_hash)
                     self.socket.sendto(fin_ack_header, addr)
                     with self.lock: 
                         self.fin = True 
@@ -85,7 +94,8 @@ class Streamer:
         for i in range(0, len(data_bytes), 1472 - HEADER_SIZE):
             offset = min(i + (1472 - HEADER_SIZE), len(data_bytes))
             payload = data_bytes[i:offset]
-            header = struct.pack(HEADER_FORMAT, 0, self.send_seq) # 0 indicate this packet is data, not ack
+            hash = hashlib.md5(payload).digest()
+            header = struct.pack(HEADER_FORMAT, 0, self.send_seq, hash) # 0 indicate this packet is data, not ack
             packet = header + payload
 
             # Timeout for waiting for ACKs
@@ -123,7 +133,8 @@ class Streamer:
            the necessary ACKs and retransmissions"""
         # Send a FIN packet
         self.fin_ack = False
-        fin_header = struct.pack(HEADER_FORMAT, 2, self.send_seq)
+        empty_hash = hashlib.md5(b'').digest()
+        fin_header = struct.pack(HEADER_FORMAT, 2, self.send_seq, empty_hash)
         timeout = 0.25
         start_time = time.time()
         self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
